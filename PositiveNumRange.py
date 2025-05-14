39fc869ad071fac47ce8e34144d525ff53871afe
nanya2io <ananyask25@gmail.com>
2025-05-14 10:36:10 +0530

    Create PositiveNumRange.py
    used to print all the positive numbers from the given set of numbers  in a range

---
+++ PositiveNumRange.py
@@ -0,0 +1,15 @@
+list1 = [12, -7, 5, 64, -14]
+print("Input: list1 =", list1)
+print("Output:", end=' ')
+for num in list1:
+    if num > 0:
+        print(num, end=', ')
+print()
+
+list2 = [12, 14, -95, 3]
+print("Input: list2 =", list2)
+print("Output:", end='')
+for num in list2:
+    if num > 0:
+       print(num, end='')
+print()
